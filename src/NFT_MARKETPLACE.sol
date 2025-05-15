d2931435e1ce7acddb3db3e994af991541bc7a8d
BRYAN738 <onyangobryan8@gmail.com>
2025-05-15 22:01:42 +0300

    changes

--- src/NFT_MARKETPLACE.sol
+++ src/NFT_MARKETPLACE.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 import {ERC721URIStorage, ERC721} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
 
 contract NFT is ERC721URIStorage {
     address public owner;
